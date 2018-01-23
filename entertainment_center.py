b12a6aaf2a5489baa2b7d3fd98f8caef5edf5e04
Babak Shahian Jahromi <BabakShah@users.noreply.github.com>
2018-01-23 16:21:11 -0600

    Add comments to the code.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,8 @@
 # Project: Movies Website
-
 import media
 import fresh_tomatoes
 
+# Creating three instances of the class Movie: toy_story, avatar and lucy
 toy_story = media.Movie("Toy Story",
 	"https://1.bp.blogspot.com/-4CEJ24flM5U/T-ePLTwLdyI/AAAAAAAAHcQ/GGYVN8SVxG0/s1600/Toy+Story+%281995%29+1.jpg",
 	"https://www.youtube.com/watch?v=KYz2wyBy3kc",
@@ -21,5 +21,11 @@ lucy = media.Movie("Lucy",
 	"Luc Besson",
 	"25 July 2014 (USA)")
 
+# Storing the movie instances in an array list
 movies = [toy_story, avatar, lucy]
+
+""" Calling open_movies_page() function from fresh_tomatoes.py file 
+	to display the movies on a webpage. We pass the movies array (list
+	of movies) as a parameter of the function.
+"""
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,28 @@
 # Project: Movies Website
-
 import webbrowser
 
 class Movie():
+	"""This class creates attributes of movies.
 
+	Attributes:
+		title (str): Movie's title.
+		poster_image_url (str): Movie's poster.
+		trailer_youtube_url (str): Movie's trailer.
+		screen_writer (str): Movie's writer.
+		launch_date (str): Movie's launch date.
+
+	Methods:
+		__init__ method: initializes the class.
+	"""
     def __init__(self,title,poster,trailer,writer,date):
-        
+        """initializes the class.
+
+        Args:
+            Movie's title, poster, trailer, writer and launch date
+
+        Returns:
+        	Nothing.
+        """
         self.title = title
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
